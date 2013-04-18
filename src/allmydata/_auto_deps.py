5e836b19ed4f7bc022db121cfdd89a7d67f9bc49
Itamar Turner-Trauring <itamar@futurefoundries.com>
2013-04-18 14:37:22 -0400

    Add oauth2client to requirements.

--- src/allmydata/_auto_deps.py
+++ src/allmydata/_auto_deps.py
@@ -67,9 +67,11 @@ install_requires = [
 
     # needed for cloud backend
     "txAWS == 0.2.1.post5",
+    "oauth2client == 1.1.0",
 
     # Will be needed to test web apps, but not yet. See #1001.
     #"windmill >= 1.3",
+
 ]
 
 # Includes some indirect dependencies, but does not include allmydata.
@@ -90,6 +92,7 @@ package_imports = [
     ('pyasn1',          'pyasn1'),
     ('mock',            'mock'),
     ('txAWS',           'txaws'),
+    ('oauth2client',    'oauth2client'),
     ('python-dateutil', 'dateutil'),
 ]
 
