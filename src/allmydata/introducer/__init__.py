53c384a80d227855a50824265343788162d4ccdd
Leif Ryge <leif@synthesize.us>
2014-06-28 22:10:08 +0000

    truckee68 has been merged into truckee
    This commit removes everything, to prevent people from accidentally continuing
    to install truckee68 now that it being maintained in the truckee branch.

--- src/allmydata/introducer/__init__.py
+++
@@ -1,9 +0,0 @@
-
-# This is for compatibilty with old .tac files, which reference
-# allmydata.introducer.IntroducerNode
-
-from allmydata.introducer.server import IntroducerNode
-
-# hush pyflakes
-_unused = [IntroducerNode]
-del _unused
